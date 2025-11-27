5c7ed9bf07109d85efd7863b848ff2a527fd7778
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-11-27 08:41:18 +0200

    Created button and itfunctionality and, created basic divs in menu

---
+++ src/menu.js
@@ -0,0 +1,30 @@
+const menuButton = (function() {
+    const navBar = document.querySelector('nav');
+    const menuBtn = document.createElement('button');
+
+    menuBtn.classList.add('menu');
+    menuBtn.textContent = 'Menu';
+
+    navBar.appendChild(menuBtn);
+
+    menuBtn.addEventListener('click', menuPage);
+})();
+
+function menuPage() {
+    const content = document.querySelector('#content');
+    const title = document.createElement('h1');
+    const menuSides = document.createElement('div');
+    const sidesTitle = document.createElement('h2');
+
+    title.classList.add('title');
+    title.textContent = 'Menu';
+    menuSides.classList.add('sides');
+    sidesTitle.textContent = 'Side dishes';
+
+    content.innerHTML = '';
+    
+    content.appendChild(title);
+    content.appendChild(menuSides);
+    menuSides.appendChild(sidesTitle);
+    
+};
