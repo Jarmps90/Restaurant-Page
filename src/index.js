a0faa8f7e239a184a89005378bb42c0c3f28035d
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-11-28 09:03:11 +0200

    Added contact.js to import list

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-import './main.css';
-import './menu.js'
+import "./main.css";
+import "./menu.js";
+import "./contact.js";
 
+const homeButton = (function () {
+  const navBar = document.querySelector("nav");
+  const homeBtn = document.createElement("button");
 
+  homeBtn.classList.add("home");
+  homeBtn.textContent = "Home";
 
-const homeButton = (function() {
-    const navBar = document.querySelector('nav');
-    const homeBtn = document.createElement('button');
+  navBar.appendChild(homeBtn);
 
-    homeBtn.classList.add('home');
-    homeBtn.textContent = 'Home';
-
-    navBar.appendChild(homeBtn);
-
-    homeBtn.addEventListener('click', generateIndexPage);
-
+  homeBtn.addEventListener("click", generateIndexPage);
 })();
 
 function generateIndexPage() {
-    const content = document.querySelector('#content');
-    const titleDiv = document.createElement('h1');
-    const textDiv = document.createElement('div');
+  const content = document.querySelector("#content");
+  const titleDiv = document.createElement("h1");
+  const textDiv = document.createElement("div");
 
-    content.innerHTML = '';
+  content.innerHTML = "";
 
-    titleDiv.classList.add('title');
-    textDiv.classList.add('text');
+  titleDiv.classList.add("title");
+  textDiv.classList.add("text");
 
-    titleDiv.textContent = 'Welcome to our restaurant page';
-    textDiv.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia praesentium quod fugiat explicabo animi ut quaerat ullam. Voluptas sed exercitationem excepturi qui quae quaerat ipsum cumque sunt doloremque velit!';
+  titleDiv.textContent = "Welcome to our restaurant page";
+  textDiv.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia praesentium quod fugiat explicabo animi ut quaerat ullam. Voluptas sed exercitationem excepturi qui quae quaerat ipsum cumque sunt doloremque velit!";
 
-    content.appendChild(titleDiv);
-    content.appendChild(textDiv);
-};
+  content.appendChild(titleDiv);
+  content.appendChild(textDiv);
+}
 
 generateIndexPage();
