c083309a0551fecce9c234b444f673a945510037
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-11-28 09:10:14 +0200

    Up

--- src/menu.js
+++ src/menu.js
@@ -1,30 +1,29 @@
-const menuButton = (function() {
-    const navBar = document.querySelector('nav');
-    const menuBtn = document.createElement('button');
+const menuButton = (function () {
+  const navBar = document.querySelector("nav");
+  const menuBtn = document.createElement("button");
 
-    menuBtn.classList.add('menu');
-    menuBtn.textContent = 'Menu';
+  menuBtn.classList.add("menu");
+  menuBtn.textContent = "Menu";
 
-    navBar.appendChild(menuBtn);
+  navBar.appendChild(menuBtn);
 
-    menuBtn.addEventListener('click', menuPage);
+  menuBtn.addEventListener("click", menuPage);
 })();
 
 function menuPage() {
-    const content = document.querySelector('#content');
-    const title = document.createElement('h1');
-    const menuSides = document.createElement('div');
-    const sidesTitle = document.createElement('h2');
+  const content = document.querySelector("#content");
+  const title = document.createElement("h1");
+  const menuSides = document.createElement("div");
+  const sidesTitle = document.createElement("h2");
 
-    title.classList.add('title');
-    title.textContent = 'Menu';
-    menuSides.classList.add('sides');
-    sidesTitle.textContent = 'Side dishes';
+  title.classList.add("title");
+  title.textContent = "Menu";
+  menuSides.classList.add("sides");
+  sidesTitle.textContent = "Side dishes";
 
-    content.innerHTML = '';
-    
-    content.appendChild(title);
-    content.appendChild(menuSides);
-    menuSides.appendChild(sidesTitle);
-    
-};
+  content.innerHTML = "";
+
+  content.appendChild(title);
+  content.appendChild(menuSides);
+  menuSides.appendChild(sidesTitle);
+}
