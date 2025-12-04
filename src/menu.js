ec50929ebc1eaf0e7dd9b0eb3912555499b38bf4
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-04 20:00:15 +0200

    created function that adds menu items divs and created class to add menu items

--- src/menu.js
+++ src/menu.js
@@ -22,4 +22,47 @@ function menuPage() {
 
   content.appendChild(title);
   content.appendChild(menuDishes);
+  createDish();
+};
+
+class Dish {
+  constructor(title, text, img) {
+    this.title = title;
+    this.text = text;
+    this.img = img;
+  };
+};
+
+function createDish() {
+  const dishes = [];
+  const beefWellington = new Dish('Beef Wellington', 'Baked steak dish made out of fillet steak and duxelles wrapped in shortcrust pastry', 'img');
+  const chineseBeefStirFry = new Dish('Chinese Beef Stir Fry', 'This Beef Stir Fry is made with a simple but super flavourful, classic Chinese stir fry sauce.', 'img');
+
+  dishes.push(beefWellington, chineseBeefStirFry);
+
+  const displayDishes = () => {
+    for (let i = 0; i < dishes.length; i++) {
+      const menu = document.querySelector('.dishes');
+      const outerDiv = document.createElement('div');
+      const innerDiv = document.createElement('div');
+      const title = document.createElement('h2');
+      const text = document.createElement('p');
+      const img = document.createElement('div');
+
+      outerDiv.classList.add('outer');
+      innerDiv.classList.add('inner');
+      title.textContent = dishes[i].title;
+      text.textContent = dishes[i].text
+
+      innerDiv.appendChild(title);
+      innerDiv.appendChild(text);
+      outerDiv.appendChild(innerDiv);
+      outerDiv.appendChild(img);
+      menu.appendChild(outerDiv);
+
+    };
+  };
+
+  displayDishes();
+
 };
