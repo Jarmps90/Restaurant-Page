b575b83f9fa2fea50f5a470aed2cc77ba9b451d5
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-07 20:50:48 +0200

    Added divs to seperate logo and text

--- src/index.js
+++ src/index.js
@@ -24,19 +24,24 @@ function generateIndexPage() {
   const firstHalf = document.createElement('p');
   const secondHalf = document.createElement('p');
   const logoDiv = document.createElement('div');
-
+  const logo = document.createElement('div');
+  const headerText = document.createElement('div');
   content.innerHTML = "";
 
   titleDiv.classList.add("title");
   textDiv.classList.add("text");
-  logoDiv.classList.add('logo');
+  logoDiv.classList.add('logoDiv');
+  logo.classList.add('logo');
+  headerText.classList.add('headerText')
 
-  logoDiv.textContent = 'It’s time to enjoy the finer things in life.'
+  headerText.textContent = 'It’s time to enjoy the finer things in life.'
   titleDiv.textContent = "Welcome to Beef n resto";
   firstHalf.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia praesentium quod fugiat explicabo animi ut quaerat ullam. Voluptas sed exercitationem excepturi qui quae quaerat ipsum cumque sunt doloremque velit!'
   secondHalf.textContent = ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias odio tenetur fuga dolor quidem recusandae earum temporibus quam cupiditate exercitationem.';
 
   header.appendChild(logoDiv);
+  logoDiv.appendChild(headerText);
+  logoDiv.appendChild(logo)
   content.appendChild(titleDiv);
   content.appendChild(textDiv);
   textDiv.appendChild(firstHalf);
