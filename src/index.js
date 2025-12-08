35ed9c1d79e805d2c4602638b4e5d7d50bef5f38
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-08 08:23:43 +0200

    Reranged code for better readability,  created inner and outter divs to control placement and styling

--- src/index.js
+++ src/index.js
@@ -19,33 +19,47 @@ const homeButton = (function () {
 function generateIndexPage() {
   const content = document.querySelector("#content");
   const header = document.querySelector('header');
-  const titleDiv = document.createElement("h1");
-  const textDiv = document.createElement("div");
-  const firstHalf = document.createElement('p');
-  const secondHalf = document.createElement('p');
+
   const logoDiv = document.createElement('div');
   const logo = document.createElement('div');
   const headerText = document.createElement('div');
+
+  const outerContainer = document.createElement('div')
+  const innerContainer = document.createElement('div');
+  const title = document.createElement("h1");
+  const text = document.createElement("div");
+  const firstHalf = document.createElement('p');
+  const secondHalf = document.createElement('p');
+
+
+
   content.innerHTML = "";
 
-  titleDiv.classList.add("title");
-  textDiv.classList.add("text");
+  outerContainer.classList.add('mainOuter');
+  innerContainer.classList.add('mainInner');
+  title.classList.add("title");
+  text.classList.add("text");
+
   logoDiv.classList.add('logoDiv');
   logo.classList.add('logo');
   headerText.classList.add('headerText')
 
   headerText.textContent = 'It’s time to enjoy the finer things in life.'
-  titleDiv.textContent = "Welcome to Beef n resto";
+
+  title.textContent = "Welcome to Beef n resto";
   firstHalf.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet officia praesentium quod fugiat explicabo animi ut quaerat ullam. Voluptas sed exercitationem excepturi qui quae quaerat ipsum cumque sunt doloremque velit!'
   secondHalf.textContent = ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias odio tenetur fuga dolor quidem recusandae earum temporibus quam cupiditate exercitationem.';
 
   header.appendChild(logoDiv);
   logoDiv.appendChild(headerText);
   logoDiv.appendChild(logo)
-  content.appendChild(titleDiv);
-  content.appendChild(textDiv);
-  textDiv.appendChild(firstHalf);
-  textDiv.appendChild(secondHalf);
+
+  innerContainer.appendChild(title);
+  innerContainer.appendChild(text);
+  text.appendChild(firstHalf);
+  text.appendChild(secondHalf);
+  outerContainer.appendChild(innerContainer);
+  content.appendChild(outerContainer);
 }
 
 function stickyNav() {
