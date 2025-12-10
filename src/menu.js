93d12584fdcaaa643c4694f60287513820249101
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-10 09:36:45 +0200

    Imported images, changed some element classes and, added images.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,6 @@
+import beefWellingtonImg from './resorces/loija-nguyen-Lf2dvyS-d2E-unsplash.jpg';
+import beefStirFryImg from './resorces/davis-dai-qKPKDb4_W24-unsplash.jpg';
+
 const menuButton = (function () {
   const navBar = document.querySelector("nav");
   const menuBtn = document.createElement("button");
@@ -15,6 +18,7 @@ function menuPage() {
   const title = document.createElement("h1");
   const menuDishes = document.createElement("div");
 
+  title.classList.add('menu-title');
   title.textContent = "Menu";
   menuDishes.classList.add("dishes");
 
@@ -35,8 +39,8 @@ class Dish {
 
 function createDish() {
   const dishes = [];
-  const beefWellington = new Dish('Beef Wellington', 'Baked steak dish made out of fillet steak and duxelles wrapped in shortcrust pastry', 'img');
-  const chineseBeefStirFry = new Dish('Chinese Beef Stir Fry', 'This Beef Stir Fry is made with a simple but super flavourful, classic Chinese stir fry sauce.', 'img');
+  const beefWellington = new Dish('Beef Wellington', 'Baked steak dish made out of fillet steak and duxelles wrapped in shortcrust pastry', beefWellingtonImg);
+  const chineseBeefStirFry = new Dish('Beef Stir Fry', 'This Beef Stir Fry is made with a simple but super flavourful, classic stir fry sauce.', beefStirFryImg);
 
   dishes.push(beefWellington, chineseBeefStirFry);
 
@@ -47,13 +51,15 @@ function createDish() {
       const menuInner = document.createElement('div');
       const title = document.createElement('h2');
       const text = document.createElement('p');
-      const img = document.createElement('div');
+      const img = document.createElement('img');
 
+      title.classList.add('dish-title');
       menuOuter.classList.add('menuOuter');
       menuInner.classList.add('MenuInner');
       img.classList.add('img');
       title.textContent = dishes[i].title;
       text.textContent = dishes[i].text
+      img.src = dishes[i].img;
 
       menuInner.appendChild(title);
       menuInner.appendChild(text);
